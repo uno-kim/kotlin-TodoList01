7cf1abc8a7141b13c366ff81863f4e1ba8905436
Min Sung Kim <ms.uno.kim@gmail.com>
2018-12-08 15:08:36 +0900

    Initial commit

---
+++ app/src/main/java/com/unokim/kotlin/todolist/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.unokim.kotlin.todolist
+
+import android.app.Application
+import io.realm.Realm
+
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
